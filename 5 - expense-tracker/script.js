8e8ecf2864a77deedc06764336b8a34e52b0af20
th_linux <holayinkajr@gmail.com>
2020-11-27 14:51:51 -0800

    Added Transaction functions

--- 5 - expense-tracker/script.js
+++ 5 - expense-tracker/script.js
@@ -1,3 +1,4 @@
+//VIEW
 const balance = document.getElementById('balance');
 const money_plus = document.getElementById('money-plus');
 const money_minus = document.getElementById('money-minus');
@@ -6,4 +7,65 @@ const form = document.getElementById('form');
 const text_space = document.getElementById('text');
 const amount_space = document.getElementById('amount');
 
+//MODEL
+let transactions = [];
 
+
+
+//CONTROLLER
+
+function randomID() {
+  return Math.floor(Math.random() * 100000000);
+}
+
+function addTransactionDOM(transaction) {
+  const item = document.createElement('li');//check
+
+  item.innerHTML = `
+    ${transaction.text}<span>${transaction.amount}</span>
+    <button class="delete-btn" onclick="removeTransaction()">x</button>
+    `
+  history_list.appendChild(item);
+}
+
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+  const total_amount = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+  .filter(item => item > 0)
+  .reduce((acc, item) => (acc += item), 0)
+  .toFixed(2);
+
+  const expense = amounts
+  .filter(item => item < 0)
+  .reduce((acc, item) => (acc += item), 0)
+  .toFixed(2);//check
+
+  balance.innerText = `${total_amount}`
+  money_plus.innerText = `${income}`
+  money_minus.innerText = `${expense}`
+}
+
+function removeTransaction(id) {
+  
+}
+
+function addTransaction() {
+  
+  if (text_space.value.trim() === '' || amount_space.value.trim() === '') {
+    alert('Please add a text and amount')
+  } else {
+    const transaction = {
+      id: randomID(),
+      text: text_space.value,
+      amount: text_space.amount
+    };
+
+    transactions.push(transaction)
+
+    addTransactionDOM(transaction);
+
+    updateValues();
+  }
+}
