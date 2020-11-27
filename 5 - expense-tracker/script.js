c19c28f30fe27895af718845e10370a26d41e6c6
th_linux <holayinkajr@gmail.com>
2020-11-27 13:15:38 -0800

    Added HTML, CSS and JS files

---
+++ 5 - expense-tracker/script.js
@@ -0,0 +1,9 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const history_list = document.getElementById('list');
+const form = document.getElementById('form');
+const text_space = document.getElementById('text');
+const amount_space = document.getElementById('amount');
+
+
