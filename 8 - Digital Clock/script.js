5a7363600d27c66c4a7212e3697830af0e035500
th_linux <holayinkajr@gmail.com>
2020-12-02 14:17:37 -0800

    added files

---
+++ 8 - Digital Clock/script.js
@@ -0,0 +1,9 @@
+
+let clock = document.getElementById("clock");
+
+setInterval(() => {
+    const date = new Date();
+    const time = date.toLocaleTimeString();
+    clock.innerHTML = time;
+}, 1000);
+
