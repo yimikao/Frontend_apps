410a889c5bc40c8f537437edb32678b982e4547d
th_linux <holayinkajr@gmail.com>
2020-12-03 14:39:02 -0800

    added app script

---
+++ 10 - Interactive Quiz Project/script.js
@@ -0,0 +1,54 @@
+//MODEL
+var questions = [
+    {
+        question: 'What is the baby of a moth known as?',
+        choices: ['baby', 'infant', 'kit', 'larva'],
+        correctAnswer: 3
+    },
+    {
+        question: 'What is the adult of a kid called?',
+        choices: ['calf', 'doe', 'goat', 'chick'],
+        correctAnswer: 2
+    },
+    {
+        question: 'What is the young of buffalo called?',
+        choices: ['calf', 'baby', 'pup', 'cow'],
+        correctAnswer: 0
+    },
+    {
+        question: 'What is a baby alligator called?',
+        choices: ['baby', 'gator', 'hatchling', 'calf'],
+        correctAnswer: 1
+    },
+    {
+        question: 'What is a baby goose called?',
+        choices: ['gooser', 'gosling', 'gup', 'pup'],
+        correctAnswer: 1
+    }
+];
+
+//VIEW
+let questionSpace = document.querySelector('.question');
+let choiceSpace = document.querySelector('.choiceList');
+let msgSpace = document.querySelector('.quizMessage');
+let resultSpace = document.querySelector('.result');
+let button = document.querySelector('.nextButton');
+
+//CONTROLLER
+
+function DisplayCurrentQuestion() {
+    
+}
+function resetQuiz() {
+    
+}
+function diplayScore() {
+
+}
+function hideScore() {
+    
+}
+
+window.addEventListener('DOMContentLoaded', function (e) {
+    
+})
