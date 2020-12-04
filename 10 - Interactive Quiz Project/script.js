07abdc4b89e1e0e81615dea2cf69e060fd03b4eb
th_linux <holayinkajr@gmail.com>
2020-12-04 11:17:53 -0800

    completed app script

--- 10 - Interactive Quiz Project/script.js
+++ 10 - Interactive Quiz Project/script.js
@@ -35,20 +35,84 @@ let resultSpace = document.querySelector('.result');
 let button = document.querySelector('.nextButton');
 
 //CONTROLLER
+let currentQuestion = 0;
+let correctAnswers = 0;
+let numChoices = questions[currentQuestion].choices.length;
+let quizOver = false;
 
 function DisplayCurrentQuestion() {
-    
+    let question = questions[currentQuestion].question;
+    questionSpace.innerHTML = question;
+    choiceSpace.innerHTML = "";
+    let choice;
+    for (i = 0; i < numChoices; i++) {
+        choice = questions[currentQuestion].choices[i];
+        let li = document.createElement('li');
+        li.innerHTML = `<li><input type="radio" value=${i}/>${choice}</li>`
+
+        choiceSpace.appendChild(li);
+    }
 }
 function resetQuiz() {
-    
+    currentQuestion = 0;
+    correctAnswers = 0;
+    hideScore();
 }
-function diplayScore() {
 
-}
 function hideScore() {
-    
+    resultSpace.style.display = 'none'
 }
 
-window.addEventListener('DOMContentLoaded', function (e) {
-    
-})
+window.addEventListener('DOMContentLoaded', function () {
+    DisplayCurrentQuestion();
+
+    msgSpace.style.display = 'none';
+
+    button.addEventListener('click', function () {
+        //this is the beginning of the quiz
+        //state(quizOver is false here,and its value is negated to allow condition pass thru)
+        if (!quizOver) {
+            let checkedOption = document.querySelector('input[type=radio]:checked');
+            if (checkedOption === null) {
+                msgSpace.innerHTML = 'Please select an answer';
+                msgSpace.style.display = 'block';
+            }else {
+                msgSpace.style.display = 'none';
+                if (parseInt(checkedOption.value) === questions[currentQuestion].correctAnswer) {
+                    correctAnswers++;
+                    console.log(correctAnswers)
+                }
+                currentQuestion++;
+
+                //if questions are still remaining
+                if (currentQuestion < questions.length) {
+                    DisplayCurrentQuestion();
+                }else {
+                    //if questions have finished
+                    //quizOver will be true
+                    displayScore();
+                    document.querySelector('.nextButton').innerText = 'Play Again?';
+                    quizOver = true;
+                }
+            }
+                
+        }
+        else {
+            //if you click playagain from above(!quizOver = false, there running this else section)
+            //quiz will be initialized
+            quizOver = false;
+            document.querySelector('.nextButton').innerText = 'Next Question';
+            resetQuiz();
+            displayCurrentQuestion();
+            hideScore(); 
+            
+        }
+    });
+
+});
+
+
+function displayScore() {
+    resultSpace.innerHTML = `You scored ${correctAnswers} out of ${questions.length}`;
+    resultSpace.style.display = 'block';
+}
